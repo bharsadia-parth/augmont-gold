6268f2510b8a0eaf55b5fbfb976303d1c78ed37c
BharsadiaParth <parth@iide.co>
2025-04-22 22:50:38 +0530

    tll now

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,61 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import client from "../server";
+import { users } from "../../generated/prisma";
 
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const limit = req.query.limit ? parseInt(req.query.limit as string) : 10;
+    const offset = req.query.offset ? parseInt(req.query.offset as string) : 0;
 
-export const getUser = (req: Request, res: Response) => {
-    console.log(req.url)
-    res.status(200).json({"name": "krishna"});
+    const users = await client.users.findMany({
+      skip: offset,
+      take: limit,
+    });
+
+    res.status(200).json({ data: users });
+  } catch (e) {
+    console.error("Failed to fetch users:", e);
+    res.status(500).json({ error: "Failed to fetch users" });
+  }
+};
+
+export const getUserById = async  (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id) ?? 0;
+        const user = await client.users.findFirst({where:{id: id}})
+        if(!user){
+            res.status(400).json({err: `User with id '${id}' not found`});
+        }
+        res.status(200).json({data:user});
+      } catch (e) {
+        console.error("Failed to fetch user", e)
+        res.status(500).json({ error: "Failed to fetch users" });
+      }
+}
+
+export const deleteUserById = async  (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id) ?? 0;
+        const result = await client.users.delete({where: {id}});
+        res.status(200).json({data:`user deleted with id '${result.id}'`});
+      } catch (e) {
+        console.error("Failed to delete user", e)
+        res.status(500).json({ error: "Failed to delete user" });
+      }
+}
+
+export const updateUserById = async  (req: Request, res: Response) => {
+    try {
+        const id = parseInt(req.params.id) ?? 0;
+        const data: Partial<users> = req.body;
+        delete data.id;
+        const result = await client.users.update({
+            data: data,
+            where: {id}
+        });
+        res.status(200).json({data:`user updated with id '${result.id}'`, user: result});
+      } catch (e) {
+        console.error("Failed to update user", e)
+        res.status(500).json({ error: "Failed to update user" });
+      }
 }
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,9 @@
+import { getUsers, getUserById, deleteUserById, updateUserById } from "../controllers/user.controller";
 import express from "express";
-import { getUser } from "../controllers/user.controller";
 
 const userRoutes = express.Router();
-userRoutes.get("/", getUser)
+userRoutes.get("/", getUsers)
+userRoutes.get("/:id", getUserById)
+userRoutes.delete("/:id", deleteUserById)
+userRoutes.patch("/:id", updateUserById)
 export default userRoutes;
